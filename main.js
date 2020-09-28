8670f7497b86c44c3cbb2f09e7e2fc7d3cf64a50
khushalreddy <40416862+khushalreddy@users.noreply.github.com>
2020-09-28 09:22:36 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const number = document.getElementById('Number');
+const submit = document.getElementById('submit');
+function clicked()
+{
+    const key = name.value;
+    const value = email.value;
+    const num = number.value;
+    console.log(key);
+    console.log(value);
+    console.log(num);
+
+if(key && value)
+{
+    localStorage.setItem(key,value);
+}
+ window.location.href="/s.html";
+   return false;
+};
