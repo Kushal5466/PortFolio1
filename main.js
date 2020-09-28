3cbd11d46a33c1b005d74e72924979cea8aaea3a
Kushal5466 <52171877+Kushal5466@users.noreply.github.com>
2020-09-28 11:59:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -16,6 +16,6 @@ if(key && value)
 {
     localStorage.setItem(key,value);
 }
- window.location.href("https://kushal5466.github.io/PortFolio1/s.html");
+ window.location.assign("https://kushal5466.github.io/PortFolio1/s.html");
   // return false;
 };
