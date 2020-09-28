37cebeb1c0e92431ad1c3cb99d249fc61871e31d
Kushal5466 <52171877+Kushal5466@users.noreply.github.com>
2020-09-28 09:55:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,14 +2,16 @@ const name = document.getElementById('name');
 const email = document.getElementById('email');
 const number = document.getElementById('Number');
 const submit = document.getElementById('submit');
-function clicked()
+function clicked(e)
 {
+    e.preventDefault();
     const key = name.value;
     const value = email.value;
     const num = number.value;
     console.log(key);
     console.log(value);
     console.log(num);
+    
 
 if(key && value)
 {
